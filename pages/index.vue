8abf97d8cc4cb76cd5d595617390cfcc17824240
Grzegorz Leoniec <greg@app-interactive.de>
2017-06-16 15:17:22 +0200

    NEXT-154 - added infinite loading

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
     <section class="container" style="position: relative">
         <section class="hc__cards" v-cloak="ready">
-            <card class="card hc__card" v-for="contribution in contributions" :post="contribution" :key="contribution.slug" @ready="updateGrid()">
+            <card class="card hc__card" v-for="contribution in contributions" :post="contribution" :key="contribution.slug" @ready="updateGrid">
                 <small slot="category">{{ contribution.type }}</small>
             </card>
         </section>
+        <infinite-loading :on-infinite="onInfinite" ref="infiniteLoading" spinner="waveDots"></infinite-loading>
         <button type="button" class="button is-success" id="hc-add-contribution" v-on:click="$router.push('/contributions/write')">
             <i class="fa fa-plus" aria-hidden="true"></i>
         </button>
@@ -15,29 +16,59 @@
   import feathers from '~plugins/feathers'
   import Bricks from 'bricks.js'
   import Card from '../components/Card'
+  import InfiniteLoading from 'vue-infinite-loading/src/components/InfiniteLoading.vue'
 
   export default {
     components: {
-      'card': Card
+      'card': Card,
+      'infinite-loading': InfiniteLoading
     },
     async asyncData () {
-      let {data} = await feathers.service('contributions').find()
+      let res = await feathers.service('contributions').find()
       return {
-        contributions: data
+        contributions: res.data,
+        limit: res.limit,
+        skip: res.skip
       }
     },
     data () {
       return {
         bricksInstance: null,
         contributions: null,
-        loading: false,
         errors: null,
-        ready: false
+        ready: false,
+        limit: null,
+        skip: 0
       }
     },
     methods: {
       updateGrid () {
         this.bricksInstance.resize(true).pack()
+      },
+      onInfinite () {
+        feathers.service('contributions').find({
+          query: {
+            '$skip': this.skip
+          }
+        }).then(res => {
+          console.log(res)
+          this.contributions = this.contributions.concat(res.data)
+          setTimeout(() => {
+            this.updateGrid()
+
+            let lastItemNum = res.data.length + res.skip
+            if (lastItemNum < res.total) {
+              // do load more
+              this.skip = res.skip + res.limit
+              this.$refs.infiniteLoading.$emit('$InfiniteLoading:loaded')
+            } else {
+              // do NOT load more
+              this.$refs.infiniteLoading.$emit('$InfiniteLoading:complete')
+            }
+          }, 100)
+        }).catch(err => {
+          throw new Error(500, err.message)
+        })
       }
     },
     mounted () {
@@ -57,7 +88,6 @@
 
       window.onload = () => {
         this.updateGrid()
-        this.bricksInstance.resize(true).pack()
       }
     }
   }
@@ -72,24 +102,23 @@
     }
 
     #hc-add-contribution {
-        cursor: pointer;
-        z-index: 50;
-        position: fixed;
-        bottom: 50px;
-        right: 50px;
-        box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1);
-
+        cursor:        pointer;
+        z-index:       50;
+        position:      fixed;
+        bottom:        50px;
+        right:         50px;
+        box-shadow:    0 2px 3px rgba(10, 10, 10, 0.1);
 
         &, &:link, &:active {
             outline: none !important;
         }
 
-        text-align: center;
-        width: 70px;
-        height: 70px;
-        padding: 10px 16px;
-        font-size: 24px;
-        line-height: 1.33;
+        text-align:    center;
+        width:         70px;
+        height:        70px;
+        padding:       10px 16px;
+        font-size:     24px;
+        line-height:   1.33;
         border-radius: 35px !important;
     }
 </style>
