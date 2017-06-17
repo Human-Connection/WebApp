05e1a5581823da4b363aa03f5a16965aaf2ee1d5
Grzegorz Leoniec <greg@app-interactive.de>
2017-06-17 18:19:51 +0200

    NEXT-156 - scoping

--- components/Author.vue
+++ components/Author.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="media" v-if="post">
+    <div class="media hc__author" v-if="post">
         <div class="media-left">
             <figure class="image">
                 <div v-if="post.user.avatar" v-bind:style="{ backgroundImage: 'url(' + post.user.avatar.small + ')' }" class="img-circle profile-image"></div>
@@ -38,42 +38,45 @@
 <style scoped lang="scss">
     @import "../assets/styles/utilities";
 
-    .profile-image {
-        border: 1px solid #ccc
-    }
+    .hc__author {
+        .profile-image {
+            border: 1px solid #ccc
+        }
 
-    small {
-        display:        block;
-        text-align:     center;
-        text-transform: uppercase;
-        padding:        5px 10px;
-        margin:         0 auto;
-    }
+        small {
+            display:        block;
+            text-align:     center;
+            text-transform: uppercase;
+            padding:        5px 10px;
+            margin:         0 auto;
+        }
 
-    .title {
-        font-size: $size-6;
-    }
+        .title {
+            font-size: $size-6;
+        }
 
-    .subtitle {
-        padding-top:3px;
-        color: $grey;
-        font-size: $size-7;
+        .subtitle {
+            padding-top:3px;
+            color: $grey;
+            font-size: $size-7;
 
-        .fa {
-            font-size: 12px;
-            margin-top:2px;
-            color: $grey-light;
+            .fa {
+                font-size: 12px;
+                margin-top:2px;
+                color: $grey-light;
+            }
+        }
+
+        .profile-image {
+            width:               36px;
+            height:              36px;
+            background-position: center;
+            background-size:     cover;
+        }
+
+        .img-circle {
+            border-radius: 50%;
         }
     }
 
-    .profile-image {
-        width:               36px;
-        height:              36px;
-        background-position: center;
-        background-size:     cover;
-    }
-
-    .img-circle {
-        border-radius: 50%;
-    }
 </style>
