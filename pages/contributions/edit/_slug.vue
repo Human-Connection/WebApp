27ccabc548f8dfd33bedd5d491bb34e323887e36
Grzegorz Leoniec <greg@app-interactive.de>
2017-06-22 23:09:26 +0200

    NEXT-169 - added contributionsForm and implemented for create and update

--- pages/contributions/edit/_slug.vue
+++ pages/contributions/edit/_slug.vue
@@ -3,49 +3,20 @@
         <div class="card column is-8 is-offset-2">
             <section class="section">
                 <h1 class="title">Edit {{ title }}</h1>
-                <form v-bind:disabled="loading">
-                    <div class="tabs is-toggle is-fullwidth">
-                        <ul>
-                            <li v-for="postType in options.postTypes" v-bind:class="{ 'is-active': postType.active }">
-                                <a v-bind:disabled="postType.disabled" class="button is-medium">
-                                    <span class="icon">
-                                        <i v-bind:class="'icon-tools-'+postType.value"></i>
-                                    </span>
-                                    <span>{{ postType.label }}</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="field">
-                        <label class="label">Title</label>
-                        <p class="control">
-                            <input class="input" v-model="form.title" type="text" placeholder="How do you call that story?" v-bind:disabled="loading">
-                        </p>
-                    </div>
-                    <div class="field">
-                        <label class="label">Content</label>
-                        <div class="control">
-                            <div class="quill-editor" v-model="form.content" v-quill:myQuillEditor="editorOption"></div>
-                        </div>
-                    </div>
-                    <div class="field has-text-right">
-                        <button class="button is-success" v-bind:class="{ 'is-loading': loading }" v-bind:disabled="loading" @click.prevent="onSubmit">Update</button>
-                    </div>
-                </form>
+                <contributions-form :data="form"></contributions-form>
             </section>
         </div>
     </div>
 </template>
 
-
 <script>
-  import NoSSR from 'vue-no-ssr'
+  import ContributionsForm from '~components/Contributions/ContributionsForm.vue'
   import feathers from '~plugins/feathers'
 
   export default {
     middleware: ['authenticated', 'owner'],
     components: {
-      'no-ssr': NoSSR
+      ContributionsForm
     },
     async asyncData ({params, error}) {
       try {
@@ -56,12 +27,7 @@
         })
         const data = res.data[0]
         return {
-          form: {
-            _id: data._id,
-            title: data.title,
-            content: data.content,
-            slug: data.slug
-          },
+          form: data,
           title: data.title
         }
       } catch (err) {
@@ -72,71 +38,7 @@
     data () {
       return {
         loading: false,
-        form: {
-          type: 'post',
-          title: '',
-          content: '',
-          language: 'de_DE',
-          visibility: 'public',
-          topics: [0, 1],
-          tags: [],
-          attachments: []
-        },
-        options: {
-          languages: [],
-          visibilities: [],
-          topics: [],
-          tags: [],
-          postTypes: [
-            {label: 'Post', value: 'post', active: true},
-            {label: 'Pro / Con', value: 'procon', disabled: false},
-            {label: 'Can Do', value: 'cando', disabled: false}
-          ]
-        },
-        editorOption: {
-          placeholder: 'What to write?',
-          modules: {
-            toolbar: [
-              ['bold', 'italic', 'underline', 'strike'],
-              ['blockquote', 'code-block']
-            ]
-          }
-        }
-      }
-    },
-    mounted () {
-      console.log('app init, my quill instance object is:', this.myQuillEditor)
-      setTimeout(() => {
-        this.content = 'i am changed'
-      }, 3000)
-    },
-    methods: {
-      onSubmit () {
-        this.loading = true
-        console.log(this.form)
-        feathers.service('contributions').patch(this.form._id, this.form)
-          .then((res) => {
-            this.loading = false
-            console.log(res)
-            this.$toast.open({
-              message: 'Your Contribution is fresh now.',
-              duration: 2000,
-              type: 'is-success'
-            })
-            this.$router.push(`/contributions/${this.form.slug}`)
-          })
-          .catch((error) => {
-            console.log(error)
-            this.loading = false
-            this.$toast.open({
-              message: error.message,
-              duration: 3000,
-              type: 'is-danger'
-            })
-          })
-      },
-      onPicture (e) {
-        alert('NEW IMAGE!')
+        form: {}
       }
     },
     head () {
--- pages/contributions/write.vue
+++ pages/contributions/write.vue
@@ -3,196 +3,19 @@
         <div class="card column is-8 is-offset-2">
             <section class="section">
                 <h1 class="title">Make a contribution</h1>
-                <form v-bind:disabled="loading">
-                    <div class="tabs is-toggle is-fullwidth">
-                        <ul>
-                            <li v-for="postType in options.postTypes" v-bind:class="{ 'is-active': postType.active }">
-                                <a v-bind:disabled="postType.disabled" class="button is-medium">
-                                    <span class="icon">
-                                        <i v-bind:class="'icon-tools-'+postType.value"></i>
-                                    </span>
-                                    <span>{{ postType.label }}</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="field">
-                        <label class="label">Title</label>
-                        <p class="control">
-                            <input class="input" v-model="form.title" type="text" placeholder="How do you call that story?" v-bind:disabled="loading">
-                        </p>
-                    </div>
-                    <div class="field">
-                        <label class="label">Content</label>
-                        <div class="control">
-                            <div class="quill-editor" v-model="form.content" v-quill:myQuillEditor="editorOption"></div>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <!--<el-input-->
-                        <!--v-model="form.content"-->
-                        <!--type="textarea"-->
-                        <!--autosize-->
-                        <!--:autosize="{ minRows: 5, maxRows: 30}">-->
-                        <!--</el-input>-->
-                    </div>
-                    <div class="field">
-                        <!--<el-select placeholder="Select language" v-model="form.language">-->
-                        <!--<slot name="languageOptions">-->
-                        <!--<el-option-->
-                        <!--v-for="item in options.languages"-->
-                        <!--:label="item.name"-->
-                        <!--:value="item.regional">-->
-                        <!--</el-option>-->
-                        <!--</slot>-->
-                        <!--</el-select>-->
-                    </div>
-                    <div class="field">
-                        <!--<el-select placeholder="Who can see this post" v-model="form.visibilityTypeId">-->
-                        <!--<slot name="visibilityOptions">-->
-                        <!--<el-option-->
-                        <!--v-for="item in options.visibilities"-->
-                        <!--:label="item.name"-->
-                        <!--:value="item.id">-->
-                        <!--</el-option>-->
-                        <!--</slot>-->
-                        <!--</el-select>-->
-                    </div>
-                    <div class="field">
-                        <!--<el-select ref="topics"-->
-                        <!--v-model="form.topics"-->
-                        <!--filterable-->
-                        <!--multiple-->
-                        <!--placeholder="Choose some topics">-->
-                        <!--<slot name="topicOptions">-->
-                        <!--<el-option-->
-                        <!--v-for="item in options.topics"-->
-                        <!--:label="item.label"-->
-                        <!--:value="item.value">-->
-                        <!--</el-option>-->
-                        <!--</slot>-->
-                        <!--</el-select>-->
-                    </div>
-                    <div class="field">
-                        <!--<el-select ref="tags"-->
-                        <!--v-model="form.tags"-->
-                        <!--allow-create-->
-                        <!--filterable-->
-                        <!--multiple-->
-                        <!--placeholder="Enter some tags">-->
-                        <!--<slot name="tagOptions">-->
-                        <!--<el-option-->
-                        <!--v-for="item in options.tags"-->
-                        <!--:label="item.label"-->
-                        <!--:value="item.value">-->
-                        <!--</el-option>-->
-                        <!--</slot>-->
-                        <!--</el-select>-->
-                    </div>
-                    <div class="field">
-                        <!--<el-upload-->
-                        <!--ref="upload"-->
-                        <!--class="upload"-->
-                        <!--drag-->
-                        <!--:auto-upload="true"-->
-                        <!--action="http://hc.dev/de/contribution/upload"-->
-                        <!--:file-list="form.attachments"-->
-                        <!--:with-credentials="true"-->
-                        <!--:data="uploadData"-->
-                        <!--multiple>-->
-                        <!--<i class="el-icon-upload"></i>-->
-                        <!--<div class="el-upload__text">Drop file here or <em>click to upload</em></div>-->
-                        <!--<div class="el-upload__tip" slot="tip">jpg/png/gif files with a size less than 2MB</div>-->
-                        <!--</el-upload>-->
-                    </div>
-                    <div class="field has-text-right">
-                        <button class="button is-success" v-bind:class="{ 'is-loading': loading }" v-bind:disabled="loading" @click.prevent="onSubmit">Publish</button>
-                    </div>
-                </form>
+                <contributions-form></contributions-form>
             </section>
         </div>
     </div>
 </template>
 
-
 <script>
-  import NoSSR from 'vue-no-ssr'
-  import feathers from '~plugins/feathers'
+  import ContributionsForm from '~components/Contributions/ContributionsForm.vue'
 
   export default {
     middleware: 'authenticated',
     components: {
-      'no-ssr': NoSSR
-    },
-    data () {
-      return {
-        loading: false,
-        form: {
-          type: 'post',
-          title: '',
-          content: '',
-          language: 'de_DE',
-          visibility: 'public',
-          topics: [0, 1],
-          tags: [],
-          attachments: []
-        },
-        options: {
-          languages: [],
-          visibilities: [],
-          topics: [],
-          tags: [],
-          postTypes: [
-            {label: 'Post', value: 'post', active: true},
-            {label: 'Pro / Con', value: 'procon', disabled: false},
-            {label: 'Can Do', value: 'cando', disabled: false}
-          ]
-        },
-        editorOption: {
-          placeholder: 'What to write?',
-          modules: {
-            toolbar: [
-              ['bold', 'italic', 'underline', 'strike'],
-              ['blockquote', 'code-block']
-            ]
-          }
-        }
-      }
-    },
-    mounted () {
-      console.log('app init, my quill insrance object is:', this.myQuillEditor)
-      setTimeout(() => {
-        this.content = 'i am changed'
-      }, 3000)
-    },
-    methods: {
-      onSubmit () {
-        this.loading = true
-        console.log(this.form)
-        feathers.service('contributions').create(this.form)
-          .then((res) => {
-            this.loading = false
-            console.log(res)
-            this.$toast.open({
-              message: 'Thanks for your contribution. You are awesome.',
-              duration: 2000,
-              type: 'is-success'
-            })
-            this.$router.push(`/contributions/${res.slug}`)
-          })
-          .catch((error) => {
-            console.log(error)
-            this.loading = false
-            this.$toast.open({
-              message: error.message,
-              duration: 3000,
-              type: 'is-danger'
-            })
-          })
-      },
-      onPicture (e) {
-        alert('NEW IMAGE!')
-      }
+      ContributionsForm
     }
   }
 </script>
