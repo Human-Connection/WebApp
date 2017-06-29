5958fafa2c9490ce011526b6f49ab35ddd83026a
Grzegorz Leoniec <greg@app-interactive.de>
2017-06-29 07:00:21 +0200

    NEXT-161 - fixed some magic

--- components/layout/VerificationBanner.vue
+++ components/layout/VerificationBanner.vue
@@ -2,7 +2,7 @@
     <section class="global-messages">
         <div class="notification is-primary" v-if="active && isAuthenticated && !isVerified">
             <button class="delete" @click="active = false"></button>
-            <b-icon icon="warning"></b-icon>Please verify your account in order to make contributions to our community: <a @click="resendVerification()">resend verification mail</a>
+            Please verify your account in order to make contributions to our community: <a @click="resendVerification()">resend verification mail</a>
         </div>
     </section>
 </template>
