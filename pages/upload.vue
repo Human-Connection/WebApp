39696eb2d17cad807e671259f505ae33e7aae491
JB <jb@visualjerk.de>
2017-07-02 00:02:30 +0200

    Basic file upload example

---
+++ pages/upload.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="container">
+        <app-upload></app-upload>
+    </section>
+</template>
+
+<script>
+  import AppUpload from '../components/Upload/Upload.vue'
+
+  export default {
+    components: {
+      'app-upload': AppUpload
+    },
+    head () {
+      return {
+        title: 'Upload'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+</style>
