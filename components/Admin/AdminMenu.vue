59c8e844fdc3055186964e2b0614d0b279338cad
JB <jb@visualjerk.de>
2017-07-20 00:02:13 +0200

    Added admin pages

---
+++ components/Admin/AdminMenu.vue
@@ -0,0 +1,79 @@
+<template>
+  <nav class="sidebar-menu">
+    <nuxt-link to="/admin" class="menu-item">
+      <span class="menu-icon">
+        <hc-icon icon="th"></hc-icon>
+      </span>
+      Dashboard
+    </nuxt-link>
+    <nuxt-link to="/admin/settings" class="menu-item">
+      <span class="menu-icon">
+        <hc-icon icon="cog"></hc-icon>
+      </span>
+      Einstellungen
+    </nuxt-link>
+    <nuxt-link to="/admin/categories" class="menu-item">
+      <span class="menu-icon">
+        <hc-icon icon="comments"></hc-icon>
+      </span>
+      Kategorien
+    </nuxt-link>
+    <nuxt-link to="/admin/users" class="menu-item">
+      <span class="menu-icon">
+        <hc-icon icon="user"></hc-icon>
+      </span>
+      Benutzer
+    </nuxt-link>
+  </nav>
+</template>
+
+
+<script>
+  export default {
+    name: 'hc-sidebar-menu'
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "~assets/styles/utilities";
+
+  $icon-padding: ($sidebar-closed-width - $sidebar-icon-width)/2;
+
+  .menu-item {
+    display: flex;
+    position: relative;
+    padding: 10px $icon-padding;
+    margin-bottom: 5px;
+    color: $grey-darker;
+    align-items: center;
+    width: $sidebar-open-width;
+    transition: all 0.1s ease-out;
+    letter-spacing: 0.05em;
+
+    &:hover {
+      background-color: $white-ter;
+    }
+
+    &.nuxt-link-exact-active {
+      background-color: $white-ter;
+    }
+  }
+
+  .menu-icon {
+    display: inline-flex;
+    margin-right: $icon-padding + 2px;
+    width: 40px;
+    height: 40px;
+    border-radius: 100%;
+    color: $grey-dark;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.2em;
+    transition: all 0.1s ease-out;
+
+    .nuxt-link-exact-active & {
+      color: $white;
+      background-color: $primary;
+    }
+  }
+</style>
