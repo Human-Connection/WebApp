d8b7ce8daea29344de3ade2f2ef8a0b59c0598be
Rainer <orlat@gmx.de>
2017-08-06 10:09:54 +0200

    NEXT-230: display arrow only if no mose over

--- components/layout/Timeline.vue
+++ components/layout/Timeline.vue
@@ -17,8 +17,9 @@
       <div v-if="contributions.length > 0" class="timeline-missing-line"></div>
       <div class="timeline-post-wrapper is-clearfix" v-if="contributions.length > 0"
         v-for="(contribution, index) in contributions">
-        <div class="timeline-post-direction" :class="oddOrEven(index)">
-          <contribution-card class="card timeline"
+        <div class="timeline-post-direction" :class="oddOrEven(index)"
+         @mouseenter="mouseenter" @mouseleave="mouseleave">
+          <contribution-card class="card timeline" :class="getarrow()"
             :post="contribution" :key="contribution.slug">
             <small slot="category">{{ contribution.type }}</small>
           </contribution-card>
@@ -36,6 +37,7 @@
   export default {
     data () {
       return {
+        hasArrow: true,
         loading: false,
         contributions: []
       }
@@ -51,6 +53,18 @@
     methods: {
       oddOrEven (index) {
         return parseInt(index) % 2 === 0 ? 'even' : 'odd'
+      },
+      mouseenter () {
+        console.log('enter')
+        this.hasArrow = false
+      },
+      mouseleave () {
+        console.log('leave')
+        this.hasArrow = true
+      },
+      getarrow () {
+        console.log('arrow')
+        return this.hasArrow ? 'arrow' : ''
       }
     },
     mounted () {
@@ -94,6 +108,29 @@ $green: hsl(78, 71%, 41%);
     width: 100%;
   }
   .timeline-post-direction {
+    .arrow {
+      position: relative;
+    }
+    .arrow:after, .arrow:before {
+      top: 50%;
+      border: solid transparent;
+      content: " ";
+      height: 0;
+      width: 0;
+      position: absolute;
+      pointer-events: none;
+    }
+    .arrow:after {
+      border-color: rgba(255, 255, 255, 0);
+      border-width: 10px;
+      margin-top: -10px;
+    }
+    .arrow:before {
+      border-color: rgba(255, 255, 255, 0);
+      border-width: 10px;
+      margin-top: -10px;
+    }
+
     width: 50%;
     &.odd {
       float: right;
@@ -104,9 +141,27 @@ $green: hsl(78, 71%, 41%);
           float: right;
         }
       }
+      .arrow:after, .arrow:before {
+        right: 100%;
+      }
+      .arrow:after {
+        border-right-color: #ffffff;
+      }
+      .arrow:before {
+        border-right-color: #ffffff;
+      }
     }
     &.even {
       border-right: 2px solid $green;
+      .arrow:after, .arrow:before {
+        left: 100%
+      }
+      .arrow:after {
+        border-left-color: #ffffff;
+      }
+      .arrow:before {
+        border-left-color: #ffffff;
+      }
     }
   }
   .timeline-intro {
