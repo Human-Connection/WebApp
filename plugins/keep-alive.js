4e4a326a00bf9f0bedbd5cdfc4ecab64275c4ca4
JB <jb@visualjerk.de>
2018-03-21 23:48:47 +0100

    Fix keep alive

--- plugins/keep-alive.js
+++ plugins/keep-alive.js
@@ -1,19 +1,25 @@
 import Vue from 'vue'
 
+let lastRoute
 const keepAliveHook = {}
 keepAliveHook.install = (Vue) => {
-  // Keep alive these pages
-  // Examples:
-  // pages/index -> index
-  // pages/contributions/_slug -> contributions/Slug
   const keepAlivePages = process.env.keepAlivePages || []
 
   Vue.mixin({
+    // Save route if this instance is a page (has metaInfo)
+    mounted () {
+      if (this.$metaInfo) {
+        lastRoute = this.$route.name
+      }
+    },
+    activated () {
+      if (this.$metaInfo) {
+        lastRoute = this.$route.name
+      }
+    },
     deactivated () {
-      const pagesRegex = /^<Pages\/([\S\s]+?).vue/gm
-      const pageMatch = pagesRegex.exec(this._name)
       // If this is a page and we don't want it to be kept alive
-      if (pageMatch && !keepAlivePages.includes(pageMatch[1])) {
+      if (this.$metaInfo && !keepAlivePages.includes(lastRoute)) {
         this.$destroy()
       }
     }
