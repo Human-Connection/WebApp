8ed95db41bf8a2106bbefc220640f9d03e8e1e1a
Grzegorz Leoniec <greg@app-interactive.de>
2018-03-30 20:06:51 +0200

    small fixes

--- components/Organizations/OrganizationsForm.vue
+++ components/Organizations/OrganizationsForm.vue
@@ -124,7 +124,8 @@
           if (this.form._id) {
             formData.isEnabled = true
 
-            await this.app.$api.service('organizations').patch(formData._id, formData)
+            await this.$api.service('organizations')
+              .patch(formData._id, formData)
             this.isLoading = false
             this.$snackbar.open({
               message: this.$t('component.organization.organizationSaveSuccess'),
