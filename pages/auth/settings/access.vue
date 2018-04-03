6f35240d007825dc3c125b0376d7635af2a0d89a
JB <jb@visualjerk.de>
2018-04-03 20:16:05 +0200

    Optimize translations / password strength

--- pages/auth/settings/access.vue
+++ pages/auth/settings/access.vue
@@ -7,7 +7,7 @@
       <p>{{ $t('auth.settings.securityWelcomeText') }}</p>
     </div>
     <hr>
-    <h3 class="title is-4">Passwort ändern</h3>
+    <h3 class="title is-4">{{ $t('auth.settings.changePassword') }}</h3>
     <div class="columns">
       <div class="column">
         <div class="field"
@@ -55,6 +55,15 @@
           </div>
           <p v-if="$v.form.passwordNewConfirm.$error" class="help is-danger">{{ $t('auth.register.validationErrorPasswordRepeat') }}</p>
         </div>
+        <div class="field">
+          <div class="password-strength-meter">
+            <div class="password-strength-meter-inner"
+              :class="strengthClass"></div>
+          </div>
+          <p class="help" v-if="this.form.passwordNew">
+            {{ $t('auth.register.passwordSecurity') }}: <strong>{{ $t(`auth.register.passwordStrength${passwordStrength}`) }}</strong>
+          </p>
+        </div>
       </div>
     </div>
     <footer class="card-footer">
@@ -73,6 +82,7 @@
   import animatable from '~/components/mixins/animatable'
   import { validationMixin } from 'vuelidate'
   import { required, sameAs, minLength } from 'vuelidate/lib/validators'
+  import zxcvbn from 'zxcvbn'
 
   export default {
     transition: 'NONE',
@@ -91,7 +101,17 @@
     computed: {
       ...mapGetters({
         user: "auth/user"
-      })
+      }),
+      /**
+       * passwordStrength is the score calculated by zxcvbn
+       * @return {Number} Password Strength Score
+       */
+      passwordStrength () {
+        return this.form.passwordNew ? zxcvbn(this.form.passwordNew).score : 0
+      },
+      strengthClass () {
+        return `strength-${this.passwordStrength}`
+      }
     },
     validations () {
       return {
@@ -160,6 +180,68 @@
   }
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
   @import "assets/styles/_animations";
+  @import "assets/styles/_variables";
+
+  .password-strength-meter {
+    position: relative;
+    height: 3px;
+    background: #DDD;
+    margin: 10px auto 20px;
+    border-radius: 3px;
+
+    &:before, &:after {
+      content: '';
+      height: inherit;
+      background: transparent;
+      display: block;
+      border-color: #FFF;
+      border-style: solid;
+      border-width: 0 6px 0 6px;
+      box-sizing: border-box;
+      position: absolute;
+      width: calc(20% + 6px);
+      z-index: 10;
+    }
+
+    &:before {
+      left: calc(20% - 6px);
+    }
+    &:after {
+      right: calc(20% - 6px);
+    }
+  }
+
+  .password-strength-meter-inner {
+    background: transparent;
+    height: inherit;
+    position: absolute;
+    width: 0;
+    border-radius: inherit;
+    transition: width 0.5s ease-in-out, background 0.25s;
+  }
+
+  .password-strength-meter-inner {
+    &.strength-0 {
+      background: darken($orange, 40%);
+      width: 20%;
+    }
+    &.strength-1 {
+      background: darken(mix($orange, $yellow, 50%), 30%);
+      width: 40%;
+    }
+    &.strength-2 {
+      background: darken($yellow, 20%);
+      width: 60%;
+    }
+    &.strength-3 {
+      background: darken($green, 10%);
+      width: 80%;
+    }
+    &.strength-4 {
+      background: $green;
+      width: 100%;
+    }
+  }
 </style>
