de2ad82801505839b99cf4ff079a69d7f6049f48
JB <jb@visualjerk.de>
2018-04-26 20:45:19 +0200

    use embeds for mentions

--- plugins/quill/mentions.js
+++ plugins/quill/mentions.js
@@ -1,16 +1,17 @@
 import { Quill } from 'vue-quill-editor'
 import { buildUserUrl } from '~/helpers/urls'
-let InlineBlot = Quill.import('blots/inline')
+let EmbedBlot = Quill.import('blots/embed')
 
 const dataIdentifier = 'data-hc-mention'
 const mentionClass = 'hc-editor-mention-blot'
-class MentionBlot extends InlineBlot {
+class MentionBlot extends EmbedBlot {
   static create (item) {
     let node = super.create()
     let url = buildUserUrl(item.slug)
     node.setAttribute('href', url)
     node.setAttribute('class', mentionClass)
     node.setAttribute(dataIdentifier, JSON.stringify(item))
+    node.insertAdjacentHTML('beforeend', item.name)
     return node
   }
 
