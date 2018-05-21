9d634e2398b76cd832a8ea0fcd25eedb359a1c20
Grzegorz Leoniec <greg@app-interactive.de>
2018-05-21 17:25:47 +0200

    improved footer on mobile

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,37 +1,35 @@
 <template>
   <footer class="footer">
     <div class="container">
-      <div class="content has-text-centered">
-        <p>
-          <hc-button
-                type="link"
-                to="https://human-connection.org"
-                target="_blank"
-                v-html="$t('humanConnectionMadeWithHeart')">
-            {{ $t('legal.imprint') }}
-          </hc-button>・
-          <hc-button
-                type="link"
-                to="/pages/imprint"
-                :prevent="true"
-                @click="$openInModal({slug: 'imprint'})">
-            {{ $t('legal.imprint') }}
-          </hc-button>・
-          <hc-button
-                type="link"
-                to="/pages/terms-and-conditions"
-                :prevent="true"
-                @click="$openInModal({slug: 'terms-and-conditions'})">
-            {{ $t('legal.termsAndConditions') }}
-          </hc-button>・
-          <hc-button
-                type="link"
-                to="/pages/privacy-policy"
-                :prevent="true"
-                @click="$openInModal({slug: 'privacy-policy'})">
-            {{ $t('legal.privacyPolicy') }}
-          </hc-button>
-        </p>
+      <div class="content">
+        <hc-button
+              type="link"
+              to="https://human-connection.org"
+              target="_blank"
+              v-html="$t('humanConnectionMadeWithHeart')">
+          {{ $t('legal.imprint') }}
+        </hc-button>・
+        <hc-button
+              type="link"
+              to="/pages/imprint"
+              :prevent="true"
+              @click="$openInModal({slug: 'imprint'})">
+          {{ $t('legal.imprint') }}
+        </hc-button>・
+        <hc-button
+              type="link"
+              to="/pages/terms-and-conditions"
+              :prevent="true"
+              @click="$openInModal({slug: 'terms-and-conditions'})">
+          {{ $t('legal.termsAndConditions') }}
+        </hc-button>・
+        <hc-button
+              type="link"
+              to="/pages/privacy-policy"
+              :prevent="true"
+              @click="$openInModal({slug: 'privacy-policy'})">
+          {{ $t('legal.privacyPolicy') }}
+        </hc-button>
       </div>
     </div>
   </footer>
@@ -63,11 +61,25 @@
 </script>
 
 <style lang="scss" scoped>
+  @import "assets/styles/utilities";
+
   footer.footer {
     position: fixed;
     bottom: 0;
     width: 100%;
-    padding: 0.8em 5em;
+    padding: 0.8em 1em;
     z-index: 50;
+
+    .content {
+      text-align: center;
+    }
+  }
+
+  @media (max-width: $tablet) {
+    footer.footer .content {
+      padding-left: 1em;
+      padding-right: 7em;
+      text-align: left;
+    }
   }
 </style>
