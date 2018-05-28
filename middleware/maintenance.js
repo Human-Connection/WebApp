8f6af3cae686ad905b541fbe7e26abe7f5bdd9da
Grzegorz Leoniec <greg@app-interactive.de>
2018-05-28 17:07:36 +0200

    added maintenence env var

--- middleware/maintenance.js
+++ middleware/maintenance.js
@@ -1,6 +1,10 @@
 
 export default async function ({ app, error, store, redirect, route }) {
   let isMaintenanceEnabled = false
+  if (Boolean(app.$env.MAINTENANCE) === true) {
+    error({ statusCode: 503, message: 'Maintenance Mode' })
+    return
+  }
   try {
     const res = await app.$api.service('status').find()
     isMaintenanceEnabled = res.maintenance === true
