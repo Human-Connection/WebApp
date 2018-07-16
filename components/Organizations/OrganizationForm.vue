f75af9be6a9e667321c264fceacafeea9dcd33b6
JB <jb@visualjerk.de>
2018-07-16 23:04:23 +0200

    Refactor orga pages

---
+++ components/Organizations/OrganizationForm.vue
@@ -0,0 +1,103 @@
+<template>
+  <div v-if="organization">
+    <div class="info-text columns">
+      <div class="column">
+        <slot name="header"></slot>
+      </div>
+      <div class="column is-3 settings-left">
+        <slot name="aside"></slot>
+      </div>
+    </div>
+    <hr>
+    <form @submit.prevent="$refs.form.validate()">
+      <div class="columns">
+        <div class="column">
+          <component ref="form"
+            :data="formData" @validate="onValidation"
+            :hideButton="true" :autoFocus="false"
+            :is="formComponent" />
+        </div>
+      </div>
+      <footer class="card-footer">
+        <hc-button
+          :isLoading="isLoading"
+          :disabled="isLoading"
+          @click.prevent="$refs.form.validate()">
+          <i class="fa fa-check"></i>
+          &nbsp;<span>{{ $t('auth.settings.saveLabel', 'Save') }}</span>
+        </hc-button>
+      </footer>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'organization-form',
+    props: {
+      formComponent: {
+        type: Object,
+        required: true
+      },
+      formData: {
+        type: Object,
+        required: true
+      },
+      updateFormData: {
+        type: Function,
+        required: true
+      },
+      organization: {
+        type: Object,
+        required: true
+      },
+      user: {
+        type: Object,
+        required: true
+      },
+      isLoading: {
+        type: Boolean,
+        default: true
+      }
+    },
+    watch: {
+      organization: {
+        handler (organization) {
+          this.updateFormData(organization)
+        },
+        deep: true
+      }
+    },
+    created () {
+      this.updateFormData(this.organization)
+    },
+    computed: {
+      canEnable () {
+        // owner (if reviewed), moderator, admin
+        return ['admin', 'moderator'].includes(this.user.role) || this.organization.reviewedBy
+      }
+    },
+    methods: {
+      onValidation (result) {
+        if (result) {
+          this.formData = Object.assign(this.formData, result)
+          this.$parent.$emit('save', this.formData)
+        } else {
+          this.$toast.open({
+            message: this.$t('auth.validation.error'),
+              type: 'is-danger'
+            })
+          this.$parent.$emit('error')
+        }
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  .settings-left {
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-end;
+  }
+</style>
--- pages/organizations/settings/index.vue
+++ pages/organizations/settings/index.vue
@@ -1,53 +1,28 @@
 <template>
-  <div v-if="organization">
-    <div class="info-text columns">
-      <div class="column">
-        <h2 class="title is-3">
-          {{ $t('component.organization.stepBasics', 'Basics') }}
-        </h2>
-        <p class="subtitle is-6">{{ $t('component.organization.settingsWelcome') }}</p>
-      </div>
-      <div class="column is-3 settings-left">
-      </div>
-    </div>
-    <hr>
-    <div class="columns">
-      <div class="column">
-        <form @submit.prevent="$refs.form.validate()">
-          <orga-form-step-1 ref="form"
-            :data="form" @validate="onValidation"
-            :hideButton="true" :autoFocus="false" />
-        </form>
-        <div class="field has-margin-top-small">
-          <div class="control">
-            <label class="label">{{ $t('component.organization.visibility') }}</label>
-            <b-switch v-model="form.isEnabled" :disabled="!canEnable">
-              <span v-if="form.isEnabled">{{ $t('info.active') }}</span>
-              <span v-else>{{ $t('info.inactive') }}</span>
-            </b-switch>
-          </div>
-        </div>
-      </div>
-    </div>
-    <footer class="card-footer">
-      <hc-button
-        :isLoading="isLoading"
-        :disabled="isLoading"
-        @click.prevent="$refs.form.validate()">
-        <i class="fa fa-check"></i>
-        &nbsp;<span>{{ $t('auth.settings.saveLabel', 'Save') }}</span>
-      </hc-button>
-    </footer>
-  </div>
+  <organization-form
+    :formComponent="$options.components.FormComponent"
+    :formData="formData"
+    :updateFormData="updateFormData"
+    :organization="organization"
+    :user="user"
+    :isLoading="isLoading">
+    <template slot="header">
+      <h2 class="title is-3">
+        {{ $t('component.organization.stepBasics', 'Basics') }}
+      </h2>
+      <p class="subtitle is-6">{{ $t('component.organization.settingsWelcome') }}</p>
+    </template>
+  </organization-form>
 </template>
 
 <script>
-  import OrgaFormStep1 from "~/components/Organizations/steps/OrgaFormStep1.vue";
+  import OrganizationForm from '~/components/Organizations/OrganizationForm'
+  import FormComponent from '~/components/Organizations/steps/OrgaFormStep1'
 
   export default {
-    mixins: [],
     components: {
-      OrgaFormStep1
+      OrganizationForm,
+      FormComponent
     },
     props: {
       organization: {
@@ -65,7 +40,7 @@
     },
     data () {
       return {
-        form: {
+        formData: {
           name: '',
           logo: '',
           isEnabled: false,
@@ -73,52 +48,18 @@
         }
       };
     },
-    watch: {
-      organization: {
-        handler () {
-          this.updateForm()
-        },
-        deep: true
-      }
-    },
-    created () {
-      this.updateForm()
-    },
-    computed: {
-      canEnable () {
-        // owner (if reviewed), moderator, admin
-        return ['admin', 'moderator'].includes(this.user.role) || this.organization.reviewedBy
-      }
-    },
     methods: {
-      updateForm () {
-        this.form = Object.assign(this.form, {
-          name: this.organization.name,
-          logo: this.organization.logo,
-          isEnabled: this.organization.isEnabled,
-          language: this.organization.language,
+      updateFormData (organization) {
+        this.formData = Object.assign(this.formData, {
+          name: organization.name,
+          logo: organization.logo,
+          isEnabled: organization.isEnabled,
+          language: organization.language,
         })
-      },
-      onValidation (result) {
-        if (result) {
-          this.form = Object.assign(this.form, result)
-          this.$emit('save', this.form)
-        } else {
-          this.$toast.open({
-            message: this.$t('auth.validation.error'),
-              type: 'is-danger'
-            })
-          this.$parent.$emit('error')
-        }
       }
     }
   };
 </script>
 
 <style lang="scss" scoped>
-  .settings-left {
-    display: flex;
-    align-items: flex-start;
-    justify-content: flex-end;
-  }
 </style>
