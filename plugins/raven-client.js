63ccb3a22cfa658a992694126625132bfac208b2
Grzegorz Leoniec <greg@app-interactive.de>
2018-07-23 14:15:02 +0200

    use build date for sentry release versioning

--- plugins/raven-client.js
+++ plugins/raven-client.js
@@ -6,7 +6,7 @@ export default ({app}) => {
   if (process.browser && app.$env.SENTRY_DNS_PUBLIC) {
     Raven
       .config(app.$env.SENTRY_DNS_PUBLIC, {
-        release: app.$env.BUILD_COMMIT,
+        release: app.$env.BUILD_DATE,
         environment: app.$env.NODE_ENV,
         tags: {
           deployed: app.$env.DEPLOY_DATE,
