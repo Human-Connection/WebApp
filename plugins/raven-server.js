656a35eccafec56affb591ea0684b7ac653b681b
Robert Schäfer <git@roschaefer.de>
2018-09-19 19:31:48 +0200

    Bugfix: process.env in non-plugin raven-server

--- plugins/raven-server.js
+++ plugins/raven-server.js
@@ -1,14 +1,16 @@
 import Raven from 'raven'
 
+// this file is directly imported
+// app is not part of context
 export default async function (app) {
   if (!process.client && process.env.SENTRY_DNS_PRIVATE) {
     // LOGGING IS ENABLED
     console.log('SENTRY LOGGING IS ENABLED')
     Raven.config(process.env.SENTRY_DNS_PRIVATE, {
-      release: app.$env.BUILD_COMMIT,
-      environment: app.$env.NODE_ENV,
+      release: process.env.BUILD_COMMIT,
+      environment: process.env.NODE_ENV,
       tags: {
-        deployed: app.$env.DEPLOY_DATE,
+        deployed: process.env.DEPLOY_DATE,
         client: true
       }
     }).install()
