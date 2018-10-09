a8dbb2eaf70efb97495f58ff443fc94433e17324
Grzegorz Leoniec <greg@app-interactive.de>
2018-10-09 18:00:44 +0200

    fix frankenstein variable

--- plugins/raven-client.js
+++ plugins/raven-client.js
@@ -9,7 +9,7 @@ export default ({app}) => {
         release: app.$env.BUILD_DATE,
         environment: app.$env.NODE_ENV,
         tags: {
-          deployed: app.$env.DEPLOY_DATE,
+          deployed: app.$env.BUILD_COMMIT,
           client: true
         }
       })
