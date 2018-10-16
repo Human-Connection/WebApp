e24795011b1138180dfa2b0c15ef1c6f5a063bd4
Robert Schäfer <git@roschaefer.de>
2018-10-16 18:16:53 +0200

    Test setup with jest and vue-test-utils

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -1,8 +1,5 @@
 const hooks = require('require-extension-hooks')
 
-// Setup browser environment
-require('browser-env')()
-
 // Setup vue files to be processed by `require-extension-hooks-vue`
 hooks('vue').plugin('vue').push()
 // Setup vue and js files to be processed by `require-extension-hooks-babel`
